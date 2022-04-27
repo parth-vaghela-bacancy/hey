6a756bea8f6d8907bb07f93c4680111aa5185158
parth-vaghela-bacancy <parth.vaghela@bacancy.com>
2022-04-27 13:11:00 +0530

    hey

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import userRouter from './routes/userRoutes.js';
+import helmet from 'helmet';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoSanitize from 'express-mongo-sanitize';
+import connectDB from './config/db.js';
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
+
+app.use('/user', userRouter);
+
+export default app;
